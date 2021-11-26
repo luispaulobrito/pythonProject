5bfc8f42fc85cd526e5c6380a2296bf39634b3ec
luispaulobrito <luiss_paullo@hotmail.com>
2021-11-26 13:41:43 -0300

    Atualização sucessor

--- ex006DObroTriploRaiz.py
+++ ex006DObroTriploRaiz.py
@@ -1,7 +1,5 @@
-import math
-
 n = int(input('Digite um número: '))
 dobro = n*2
 triplo = n*3
-raiz = math.sqrt(n)
-print('Dobro: {}, Triplo: {}, Raiz Quadrada: {}'.format(dobro, triplo, raiz))
+raiz = n ** (1/2)
+print(' Dobro: {} \n Triplo: {} \n Raiz Quadrada: {:.2f}'.format(dobro, triplo, raiz))
