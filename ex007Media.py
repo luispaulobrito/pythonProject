d5d33878adce1b33b723fff20ab5ccdbec7613fb
luispaulobrito <luiss_paullo@hotmail.com>
2021-11-26 13:47:49 -0300

    Atualização sucessor

--- ex007Media.py
+++ ex007Media.py
@@ -1,4 +1,4 @@
-n1 = float(input('DIgite a primeira nota: '))
+n1 = float(input('Digite a primeira nota: '))
 n2 = float(input('Digite a segunda nota: '))
 media = (n1+n2)/2
-print('A média entre os valores é: {}'.format(media))
+print('A média entre os valores é: {:.1f}'.format(media))
