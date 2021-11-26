3dfee7ee98d974e2ee834d7c0fdb357317838b18
luispaulobrito <luiss_paullo@hotmail.com>
2021-11-26 16:38:39 -0300

    Atualização sucessor

--- ex010ConverteRealDólar.py
+++ ex010ConverteRealDólar.py
@@ -1,3 +1,10 @@
-n = float(input('Quanto você tem na carteira em reais? '))
-dolar = n/5.61
+real = float(input('Digite o valor em reais para converter: '))
+dolar = real/5.61
+euro = real/6.33
+pesoarg = real*18.00
+pesouru = real*7.89
 print('Esse valor em dólar é: U${:.2f}'.format(dolar))
+print('Esse valor em euro é: €{:.2f}'.format(euro))
+print('Esse valor em peso argentino é: ${:.2f}'.format(pesoarg))
+print('Esse valor em peso uruguaio é: ${:.2f}'.format(pesouru))
+
