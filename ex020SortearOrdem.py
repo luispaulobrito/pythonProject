87269482335f221f5b24deb4bcddc98551abac22
luispaulobrito <luiss_paullo@hotmail.com>
2021-11-29 20:20:33 -0300

    Atualização exercícios 20

--- ex020SortearOrdem.py
+++ ex020SortearOrdem.py
@@ -4,8 +4,9 @@ a2 = str(input('Segundo aluno: '))
 a3 = str(input('Terceiro aluno: '))
 a4 = str(input('Quarto aluno: '))
 lista = [a1,a2,a3,a4]
-random.shuffle(lista)
-print('A sequência de apresentação de trabalhos será: {}'.format(lista))
+#random.shuffle(lista)
+#print('A sequência de apresentação de trabalhos será: {}'.format(lista))
 
-#embaralha = random.sample(lista,4)
-#print('A sequência de apresentação de trabalhos será: {}'.format(embaralha)) outra maneira que deu certo foi usando o sample
+embaralha = random.sample(lista,3)
+print('A sequência de apresentação de trabalhos será: {}'.format(embaralha)) #outra maneira que deu certo foi usando o sample, nele eu consigo dizer quantas eu quero que sejam sorteadas, o shuffle apenas embaralha
+
